73e58dcef5ef4045b170bc77db24ca2d21fdca15
violaBe <38519006+violaBe@users.noreply.github.com>
2018-04-28 16:39:58 +0200

    Add files via upload

--- Node.java
+++ Node.java
@@ -1,14 +1,39 @@
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Node implements Serializable {
     public String label;
-    private double x = 0;
-    private double y = 0;
-    private int id = 0;
-    private boolean thread = false;
-    private int offset = 0;
+    public double x = 0;
+    public double y = 0;
+    public int id = 0;
+    public boolean thread = false;
+    public int offset = 0;
+    public Node parent;
+    public List<Node> children = new ArrayList<>();
+    public Node leftChild;
+    public Node rightChild;
+    public double weight=0;  // len of edge to parent
 
-    public Node(String label) {
-        this.label = label;
+    public void addChild(Node node) {
+        children.add(node);
+    }
+
+//    public void addChild(Node node, boolean left){
+//        children.add(node);
+//        if (left) {
+//            leftChild = node;
+//        } else {
+//            rightChild = node;
+//        }
+//    }
+
+    @Override
+    public String toString() {
+        String out = label + "(" + x + ", " + y + ")" + "\n";
+        for (Node c : children) {
+            out += c.toString();
+        }
+        return out;
     }
 }
