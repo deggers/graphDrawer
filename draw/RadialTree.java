c366ad4baa7fcb30125461a4f6f491a48e4386f1
deggers <dustyneggers@gmail.com>
2018-05-01 00:39:52 +0200

    just an initial draft for the layout of the RadialTree Class - for now with an Node .. maybe later with a MappedTreeStructure :)

---
+++ draw/RadialTree.java
@@ -0,0 +1,12 @@
+package draw;
+
+import model.MappedTreeStructure;
+import model.Node;
+
+public class RadialTree {
+
+    public static Node processTree(Node node) {
+        // todo work that stuff out : yes, indeed!
+        return node;
+    }
+}
