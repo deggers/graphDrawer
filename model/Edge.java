0472856c73ca0e1463d735af087d25856ae0840b
deggers <dustyneggers@gmail.com>
2018-05-08 23:29:02 +0200

    not really anything :_D

--- model/Edge.java
+++ model/Edge.java
@@ -37,6 +37,7 @@ public class Edge {
     public String toString() {
         return "Edge{" + "start=" + start + ", target=" + target + ", edgeType=" + edgeType + ", weight=" + weight + '}';
     }
+
     
     
 }
