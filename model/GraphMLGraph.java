2611d257a7c90eaf616d89e65e067e0719a14a9e
deggers <dustyneggers@gmail.com>
2018-05-10 15:49:53 +0200

    simplified "getPossibleRootLabels" is now more comprehensive

--- model/GraphMLGraph.java
+++ model/GraphMLGraph.java
@@ -1,16 +1,16 @@
 package model;
 
-import java.util.*;
 import model.HelperTypes.EdgeType;
-import model.Node;
 
-public class GraphMLGraph{
+import java.util.*;
+
+public class GraphMLGraph {
     public final LinkedHashSet<Node> nodeList = new LinkedHashSet<>();
     private final LinkedHashSet<Edge> edgeList = new LinkedHashSet<>();
     private final LinkedHashSet<EdgeType> edgeTypeList = new LinkedHashSet<>();
     private LinkedHashSet<String> visitedNodesForExtractSubtreeSet = null;
-    
-    public boolean addEdgeType(String id, String attrType){
+
+    public boolean addEdgeType(String id, String attrType) {
         EdgeType et = new EdgeType(id, "double");
         if (!edgeTypeList.contains(et)) {
             edgeTypeList.add(et);
@@ -18,54 +18,29 @@ public class GraphMLGraph{
         } else {
             System.out.println("EdgeType " + et + " already in list");
             return false;
-        } 
+        }
     }
 
     public ArrayList<EdgeType> getEdgeTypes() {
         return new ArrayList<>(edgeTypeList);
     }
 
-    public List<String> getEdgeTypeLabels() {
-        List<String> returnList = new LinkedList<>();
-        for (EdgeType edgeType : edgeTypeList){
-            returnList.add(edgeType.getId());
-        }
-        return returnList;
-    }
 
     public List<String> getRelevantEdgeTypeLabels() {
         List<String> returnList = new LinkedList<>();
-            for (Node node : nodeList) {
-                if (getEdgesIn(node).isEmpty()) {
-                    for (Edge outGoingEdges : getEdgesOut(node)) {
-                            if (!returnList.contains(outGoingEdges.edgeType)) {
-                                returnList.add(outGoingEdges.edgeType);
-                                break;
-                            }
-                        }
-                    }
-                }
+        for (Node node : nodeList)
+            if (getEdgesIn(node).isEmpty()) for (Edge outGoingEdge : getEdgesOut(node))
+                if (!returnList.contains(outGoingEdge.edgeType)) returnList.add(outGoingEdge.edgeType);
         return returnList;
     }
 
-
-    public List<String> getLabelsFromRoots(String selectedEdgeType) {
-        List<String> roots = new LinkedList<>();
-        for (Node node : nodeList) {
-            if (getEdgesInWithType(node, selectedEdgeType).isEmpty()) {
-                for (Edge outGoingEdges: getEdgesOutWithType(node, selectedEdgeType))
-                    if (true) {
-                        roots.add(node.label);
-                        break;
-                    }
-            }
-        }
-        if (roots.size() == 0) {
-            roots.add("empty");
-        }
-        return roots;
+    public List<String> getPossibleRootLabels(String edgeType) {
+        List<String> possibleRoots = new LinkedList<>();
+        for (Node node : nodeList)
+            if (getEdgesIn(node).isEmpty() && getEdgesOut(node).toString().contains(edgeType)) possibleRoots.add(node.label);
+        return possibleRoots.size() > 0 ? possibleRoots : null;
     }
-    
+
     void addAllNodes(ArrayList<Node> nodes) {
         nodes.forEach(n -> {
             nodeList.add(n);
@@ -88,11 +63,11 @@ public class GraphMLGraph{
                 break;
             }
         }
-        if (!bEdgeTypeValid){ 
+        if (!bEdgeTypeValid) {
             System.out.println("Error: chosen edgeType: " + edgeType + " not in edgeTypeList");
             return null;
         }
-         root.parent = null;
+        root.parent = null;
         HashSet<Edge> temporaryEdgeSubset = new HashSet<>(); //startlabel, edge
         for (Edge edge : edgeList) { //zum schnelleren finden der Kanten des Teilbaumes
             if (edge.edgeType.equals(edgeType)) {
@@ -104,8 +79,8 @@ public class GraphMLGraph{
         extractSubtreeFromRootRecursion(root, temporaryEdgeSubset);
         return new Tree(root);
     }
-    
-    private void extractSubtreeFromRootRecursion(Node node, Set<Edge> tset){
+
+    private void extractSubtreeFromRootRecursion(Node node, Set<Edge> tset) {
 //        System.out.println("recursion at node:" + node);
         node.resetChildren();
         visitedNodesForExtractSubtreeSet.add(node.label);
@@ -120,7 +95,7 @@ public class GraphMLGraph{
             }
         }
     }
-    
+
     public List<Node> getRoots() {
         List<Node> roots = new LinkedList<>();
         for (Node node : nodeList) {
@@ -132,9 +107,9 @@ public class GraphMLGraph{
         return roots;
     }
 
-    public Node labelToNode(String label){
+    public Node labelToNode(String label) {
         Node particularNode = null;
-        for (Node node : nodeList){
+        for (Node node : nodeList) {
             if (node.label.equals(label)) particularNode = node;
         }
         if (particularNode != null) return particularNode;
@@ -158,17 +133,17 @@ public class GraphMLGraph{
         }
         return outgoingEdges;
     }
-    
+
     public List<Edge> getEdgesOutWithType(Node node, String edgeType) {
         List<Edge> outgoingEdges = new LinkedList<>();
         for (Edge e : edgeList) {
-            if (e.start.equals(node)&&e.edgeType.equals(edgeType)) {
+            if (e.start.equals(node) && e.edgeType.equals(edgeType)) {
                 outgoingEdges.add(e);
             }
         }
         return outgoingEdges;
     }
-    
+
     public List<Edge> getEdgesIn(Node node) {
         List<Edge> incomingEdges = new LinkedList<>();
         for (Edge e : edgeList) {
@@ -178,11 +153,11 @@ public class GraphMLGraph{
         }
         return incomingEdges;
     }
-    
+
     public List<Edge> getEdgesInWithType(Node node, String edgeType) {
         List<Edge> incomingEdges = new LinkedList<>();
         for (Edge e : edgeList) {
-            if (e.target.equals(node)&&e.edgeType.equals(edgeType)) {
+            if (e.target.equals(node) && e.edgeType.equals(edgeType)) {
                 incomingEdges.add(e);
             }
         }
