21b291299d0691a44d0b6bb78a05c594b0308a0d
deggers <dustyneggers@gmail.com>
2018-05-20 11:05:49 +0200

    still empty- sorry

--- draw/RadialTree.java
+++ draw/RadialTree.java
@@ -7,8 +7,8 @@ public class RadialTree extends Tree {
 
     public static Tree processTree(Tree tree) {
 
-//        calculateAngles(tree, 360,0);
-//        System.out.println("tree = " + tree);
+
+
         return tree;
     }
 }
