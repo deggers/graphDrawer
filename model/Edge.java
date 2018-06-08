0b89f303d72ab16cbed7f9bc87d907e00ead4f69
deggers <dustyneggers@gmail.com>
2018-06-08 17:15:19 +0200

    addedly mainly the hashCode and equal function.. needed somemodifaction as label is not in that class and so intelliJ did not offer that for hash automatically..

--- model/Edge.java
+++ model/Edge.java
@@ -17,6 +17,9 @@ public class Edge {
         this.target = target;
         edgeType = "none";
         weight = 1.0;
+        // set children and so
+        ((GraphNode) start).addParent((GraphNode) target);
+        ((GraphNode) target).addChild((GraphNode) start);
     }
 
     public Edge(Object start, Object target, String edgeType, Double weight) {
@@ -31,6 +34,21 @@ public class Edge {
         return this.start + " to " + this.target;
    }
 
-    
-    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Edge)) return false;
+
+        Edge edge = (Edge) o;
+
+        if (start != null ? !start.equals(edge.start) : edge.start != null) return false;
+        return target != null ? target.equals(edge.target) : edge.target == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = start != null ? start.hashCode() : 0;
+        result = 31 * result + (target != null ? target.hashCode() : 0);
+        return result;
+    }
 }
--- model/GraphNode.java
+++ model/GraphNode.java
@@ -1,9 +1,9 @@
 package model;
 
-import java.util.ArrayList;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 
 public class GraphNode extends AbstractNode {
 
@@ -24,6 +24,10 @@ public class GraphNode extends AbstractNode {
         }
     }
 
+    GraphNode(String label) {
+        super(label);
+        GraphMLType = null;
+    }
     GraphNode(String label, String type, boolean isDummyNode) {
         super(label);
         this.GraphMLType = type;
@@ -31,26 +35,41 @@ public class GraphNode extends AbstractNode {
         children = new LinkedHashSet<>();
         parents = new LinkedHashSet<>();
     }
-    GraphNode(String label) {
+    GraphNode(String label, String type, boolean isDummy, int layer) {
         super(label);
-        GraphMLType = null;
+        this.GraphMLType = type;
+        this.dummyNode = isDummy;
+        this.layer = layer;
+        children = new LinkedHashSet<>();
+        parents = new LinkedHashSet<>();
     }
+    // not sure if useful - hoped so but did not solve to create node with parent and child --dustyn
+    GraphNode(String label, String type, boolean isDummy, int layer, GraphNode parent, GraphNode children) {
+        super(label);
+        this.GraphMLType = type;
+        this.dummyNode = isDummy;
+        this.layer = layer;
+        this.children.add(children);
+        this.parents.add(parent);
+    }
+
 
     public String getLabel() {
         return this.label;
     }
-    public List<GraphNode> getChildren() {
+    public LinkedList<GraphNode> getChildren() {
         return new LinkedList<>(children);
     }
-    public List<GraphNode> getParents() {
+    public LinkedList<GraphNode> getParents() {
         return new LinkedList<>(parents);
     }
 
-    public int getLayer() {
+    int getLayer() {
         return layer;
     }
-
-    public void setLayer(int layer) {
+    String getType() {
+        return GraphMLType;}
+    void setLayer(int layer) {
         this.layer = layer;
     }
     
@@ -75,10 +94,12 @@ public class GraphNode extends AbstractNode {
 
 
 
+
     @Override
     public String toString() {
 //        return String.format("label: %s parents: %s children: %s \n", label,parentLabels(),childrenLabels());
-        return String.format("%s,level:%s", label, layer);
+//        return String.format("%s,level:%s", label, layer);
+        return String.format("%s", label);
     }
      List<String> parentLabels(){
         List<String> returnlist = new LinkedList<>();
@@ -108,6 +129,23 @@ public class GraphNode extends AbstractNode {
     boolean removeParent(GraphNode node) {
         return parents.remove(node);
     }
-    
-    
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof GraphNode)) return false;
+
+        GraphNode node = (GraphNode) o;
+
+        return label.equals(node.label);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        for (int i = 0; i < label.length(); i++)
+            hash = hash*31 + label.charAt(i);
+        return hash;
+    }
 }
