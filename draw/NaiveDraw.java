5bb454025f29682ae7085863ae9658d61b929358
deggers <dustyneggers@gmail.com>
2018-06-08 17:18:38 +0200

    nothing new

--- draw/NaiveDraw.java
+++ draw/NaiveDraw.java
@@ -8,17 +8,18 @@ public class NaiveDraw {
     public static GraphMLGraph processGraph(GraphMLGraph theGraph) throws Exception {
         String selectedEdgeType = GUIController.getInstance().getSelectedEdgeType();
         drawableGraph drawGraph = new drawableGraph(theGraph,selectedEdgeType);
-        System.out.println("drawGraph got " + drawGraph);
+//        System.out.println("drawGraph got " + drawGraph);
 
 //      1. Remove all Cycles
-        System.out.println("remove all cycles");
+//        System.out.println("remove all cycles");
         CycleBreaker.GreedyCycleRemoval(drawGraph);
-        System.out.println("cyclefree got " + drawGraph);
+//        System.out.println("cyclefree got " + drawGraph);
 
 //      2.  Layer Assignment: Vertices are assigned to layers.
-        System.out.println("layer assignment");
+//        System.out.println("layer assignment");
+//        System.out.println("drawGraph before " + drawGraph);
         AssignLayer.topologicalPath(drawGraph);
-        System.out.println("drawGraph got " + drawGraph);
+        System.out.println("drawGraph after  " + drawGraph);
 
       return theGraph;
     }
