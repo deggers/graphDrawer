98d9d3a13795b58f4821e3214c9840c8ba56c196
deggers <dustyneggers@gmail.com>
2018-07-02 12:13:32 +0200

    working on BK

--- src/Sugiyama/AssignHorizontalPosition.java
+++ src/Sugiyama/AssignHorizontalPosition.java
@@ -4,7 +4,9 @@ import structure.Edge;
 import structure.Graph;
 import structure.GraphNode;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 
 //@formatter:off
 
@@ -33,32 +35,6 @@ public class AssignHorizontalPosition {
         return graphSW;
     }
 
-    private static Graph VerticalAlignment(Graph graph, String direction) {
-        int numberOfLayer = graph.getLayerMap().keySet().size();
-        ArrayList<Edge> blockEdges = new ArrayList<>();
-
-        if (direction.contains("S")){  // top-to-bottom
-            for (int intLayer = 1; intLayer < numberOfLayer ; intLayer++) {
-                MarkTypeOneConflicts(graph, intLayer, intLayer+1);
-//                LinkedList<GraphNode> nodesOnLayer = graph.getLayerMap().get(intLayer);
-//                for (GraphNode nodeOnLayer : nodesOnLayer) {
-//                    ArrayList<GraphNode> neighbours = graph.getAdjacentNodes(nodeOnLayer, intLayer + 1);
-//                    blockEdges.add(new Edge(nodeOnLayer, getMedianNeighbour(neighbours, direction)));
-//                }
-            }
-        } else if (direction.contains("N")) {// bottom-to-top
-            for (int intLayer = numberOfLayer; intLayer > 1; intLayer--) {
-                MarkTypeOneConflicts(graph, intLayer, intLayer-1);
-                LinkedList<GraphNode> nodesOnLayer = graph.getLayerMap().get(intLayer);
-                for (GraphNode nodeOnLayer : nodesOnLayer) {
-                    ArrayList<GraphNode> neighbours = graph.getAdjacentNodes(nodeOnLayer, intLayer - 1);
-                    blockEdges.add(new Edge(nodeOnLayer, getMedianNeighbour(neighbours, direction)));
-                }
-            }
-        } else System.out.println(" what da fuck? Greetings from BK - VerticalAlignment");
-        return new Graph(graph, blockEdges);
-    }
-
     private static void MarkTypeOneConflicts(Graph graph, int layer1_int, int layer2_int) {
         LinkedList<GraphNode> layer_1 = graph.getLayerMap().get(layer1_int);
         LinkedList<GraphNode> layer_2 = graph.getLayerMap().get(layer2_int);
@@ -79,6 +55,55 @@ public class AssignHorizontalPosition {
             }
         }
     }
+
+    private static Graph VerticalAlignment(Graph graph, String direction){
+        int graphDepth = graph.getLayerMap().keySet().size();
+        LinkedHashMap<Integer,LinkedList<GraphNode>> layerMap = graph.getLayerMap();
+        int r = direction.contains("W") ? -1 : Integer.MAX_VALUE;                                          // Zeile 5,6
+        LinkedHashMap<GraphNode,GraphNode> rootMap = new LinkedHashMap<>();
+        LinkedHashMap<GraphNode,GraphNode> alignMap = new LinkedHashMap<>();
+        for(GraphNode node: graph.getNodes().values())rootMap.put(node,node);                              // Zeile 1
+        for(GraphNode node: graph.getNodes().values())alignMap.put(node,node);                             // Zeile 2
+        if (direction.contains("S")) {
+            for (int layer = 1; layer < graphDepth; layer++){                                                  // Zeile 3
+                int                     layerSize = layerMap.get(layer).size();
+                LinkedList<GraphNode>   layerList = layerMap.get(layer);
+                if (direction.contains("W")){                               // left
+                for (int k = 0; k < layerSize; k++){                        // from the mostleft position to the rightmost
+                        GraphNode                   vk_i            = layerList.get(k);
+                        ArrayList<GraphNode>        neighbors       = getNeighbors(graph, vk_i, direction);
+                        if (neighbors != null){
+                            ArrayList<GraphNode>    medianNeighbor  = new ArrayList<>();
+                            if (neighbors.size() % 2 == 0){                 // even case --> Return 2
+
+                            } else {                                        // odd case --> Return 1
+                                int d = (neighbors.size()+1)/2;             // 5 + 1 = 6; 6/2 = 3; 1,2|3|4,5
+                                medianNeighbor.add(layerList.get(median));
+                            }
+
+                        }
+                    }
+
+                } else {                                    // right
+                    for (int k = layerSize; k > 0; k--){    // from the mostright position to the mostleft
+
+                    }
+
+                }
+
+            }
+        }
+        else {
+            for (int layer = graphDepth; layer > 0; layer-- ){                                             // Zeile 4
+                // left or right?
+
+                System.out.println("going up!");
+            }
+        }
+        return graph;
+}
+
+
     private static boolean isLastNode(LinkedList<GraphNode> nodesLayer, GraphNode node){
         return node == nodesLayer.getLast();
     }
@@ -91,6 +116,7 @@ public class AssignHorizontalPosition {
         return false;
     }
 
+    // coild probably much more easier, if inner segment, incident node must be unique ..
     private static GraphNode getUpperNodeFromInnerSegment(Graph g, GraphNode u, int layer_int){
         ArrayList<GraphNode> neighbors = g.getAdjacentNodes(u, layer_int);
         for (GraphNode node : neighbors)
@@ -111,34 +137,6 @@ public class AssignHorizontalPosition {
     }
 
 
-        private static GraphNode getMedianNeighbour(ArrayList<GraphNode> neighbours, String direction) {
-        int neighbourSize = neighbours.size();
-        int median = neighbourSize % 2 != 0 ? neighbourSize / 2 : -1;
-
-        switch (direction) {
-            case "SW": // fallthrough
-            case "NW": // the both cases the left one if ambiguous
-                System.out.println("Case West");
-                if (median >= 0) { // we really have one median, easy
-                    return neighbours.get(median);
-                }
-                else { // we have not a clear median, take left one
-                    return neighbours.get((neighbourSize / 2) - 1);
-                }
-            case "SE": // fallthrough
-            case "NE": // both cases, take the right one if ambiguous
-                System.out.println("Case East");
-                if (median >= 0) { // we really have one median, easy
-                    return neighbours.get(median);
-                }
-                else { // we have not a clear median, take right one
-                    return neighbours.get( (neighbourSize / 2) );
-                }
-            default: System.out.println(" what happend here? BK greets"); break;
-        }
-        return null;
-    }
-
     public static void processNaive(Graph g) {
         int layerdepth = g.getLayerMap().keySet().size();
         for (int layer = 1; layer <= layerdepth; layer++){
@@ -151,5 +149,16 @@ public class AssignHorizontalPosition {
         }
 
     }
-
+    // has direction bias - layers need to be vertical
+    private static ArrayList<GraphNode> getNeighbors(Graph g, GraphNode node, String direction){
+        ArrayList<GraphNode> neighbors = new ArrayList<>();
+        int layer;
+        switch(direction){
+            case "predecessor":     layer = node.getLayer() - 1;     break;
+            case "successor":       layer = node.getLayer() + 1;     break;
+            default: return null;}
+        if (g.getAdjacentNodes(node, layer).size() > 0) // has neigbhors in upper layer
+            return g.getAdjacentNodes(node,layer);
+        return null;
+    }
 }
