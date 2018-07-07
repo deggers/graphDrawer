25f6de56407c387fcd8d172c0446d8286fc70f50
violaBe <38519006+violaBe@users.noreply.github.com>
2018-07-07 11:04:41 +0200

    Create BaryHelperGraph.java

---
+++ src/structure/BaryHelperGraph.java
@@ -0,0 +1,52 @@
+package structure;
+
+import Sugiyama.CrossingMin;
+import structure.GraphNode;
+
+import java.util.*;
+
+class BaryHelperGraph {
+    private int crossingsTotal = 0;
+    private LinkedHashMap<Integer, LinkedList<GraphNode>> layerMap; // key layer, entry nodes in corr order
+    private Map<Integer, BaryMatrix> baryMat=new HashMap<>();  // position in array= level of rows
+
+    void calcCrossingsTotal() {
+        for (int i = 1; i <= baryMat.size(); i++) {
+            if(i==1){
+                crossingsTotal=0;
+            }
+            crossingsTotal += baryMat.get(i).getMatCrossings();
+        }
+    }
+
+    void setBaryMat(Integer i, BaryMatrix baryMat) {
+        this.baryMat.put(i, baryMat);
+    }
+    BaryMatrix getBaryMatOnLevel(Integer level){
+        return this.baryMat.get(level);
+    }
+
+    void setLayerMap(LinkedHashMap<Integer, LinkedList<GraphNode>> layerMap){
+        this.layerMap= layerMap;
+    }
+
+    void setLayerInMap(Integer i, LinkedList<GraphNode> list){
+        this.layerMap.put(i,list );
+    }
+
+    LinkedHashMap<Integer, LinkedList<GraphNode>> getLayerMap() {
+        return layerMap;
+    }
+
+    int getCrossingsTotal(){
+        return this.crossingsTotal;
+    }
+
+    BaryHelperGraph copy(){
+        BaryHelperGraph other = new BaryHelperGraph();
+        other.layerMap = new LinkedHashMap<>(this.layerMap);
+        other.baryMat= new HashMap<>(this.baryMat);
+        other.crossingsTotal = this.crossingsTotal;
+        return other;
+    }
+}
